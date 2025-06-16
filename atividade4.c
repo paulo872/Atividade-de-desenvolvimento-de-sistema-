acd4f1ac26174d9ec1ff5e4cc41b23fbc2d82ff1
paulo872 <165043783+paulo872@users.noreply.github.com>
2025-06-16 11:59:02 -0300

    Create atividade4.c

---
+++ atividade4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int num1,num2,num3,num4,num5,num6,media;
+printf("escolha 6 numero para receber a media");
+scanf("%d",&num1);
+  scanf("%d",&num2);
+  scanf("%d",&num3);
+  scanf("%d",&num4);
+  scanf("%d",&num5);
+  scanf("%d",&num6);
+  media = num1+num2+num3+num4+num5+num6 ;
+    printf(" sua media de acordo com suas nota e :%d",media/6);
+return  0;
+}
