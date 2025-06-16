4040c7f07a745695153f0cd5a0777c58ba50c8df
paulo872 <165043783+paulo872@users.noreply.github.com>
2025-06-16 11:42:51 -0300

    Create atividade1.c

---
+++ atividade1.c
@@ -0,0 +1,10 @@
+#include &lt;stdio.h&gt;
+int main() {
+int a,b;
+printf(&quot;Insira o Numero 1: \n&quot;);
+scanf(&quot;%d&quot;, &amp;a);
+printf(&quot;Insira o Numero 2: \n&quot;);
+scanf(&quot;%d&quot;, &amp;b);
+printf(&quot;Soma: %d&quot;, a+b);
+return 0;
+}
